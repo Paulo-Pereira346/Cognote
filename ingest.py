25b1bc55497b21b34f6b50c54dc16339fb565332
Paulo-Pereira346 <paulpereira220@gmail.com>
2026-05-22 17:46:32 +0530

    Wrote a function to chunk text into chunks of specified size including overlapping of chunks

--- ingest.py
+++ ingest.py
@@ -19,7 +19,24 @@ def load_notes(folder="data"):
             file_list.append(data_dict)
         
     return file_list
+
+def chunk_text(text, chunk_size=500, overlap=50):
+    chunk_list = []
+    words = text.split()
+    start = 0
+    
+    while(start < len(words)):
+        chunk = " ".join(words[start : start + chunk_size])
+        chunk_list.append(chunk)
+        
+        start = start + chunk_size - overlap
+    
+    return chunk_list
             
 
 if __name__ == "__main__":
-    print(load_notes())   
+    notes = load_notes()
+    chunks = chunk_text(notes[0]["text"], chunk_size=30, overlap=10)
+    print(f"Number of chunks: {len(chunks)}")
+    for i, chunk in enumerate(chunks):
+        print(f"\nChunk {i+1}:\n{chunk}") 
