7cdaa299a7e1525536d83febadb20b012c8b7009
Paulo-Pereira346 <paulpereira220@gmail.com>
2026-05-23 17:58:44 +0530

    Added vector database and stored test notes with embeddings in it. Performed some testing with basic querying

--- ingest.py
+++ ingest.py
@@ -1,15 +1,14 @@
 import os
 from dotenv import load_dotenv
 from sentence_transformers import SentenceTransformer
-from numpy import dot
-from numpy.linalg import norm
+import chromadb
 
 load_dotenv()  #Load Environmental Variables
 
 #Load Model 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-
+client = chromadb.PersistentClient(path='./chroma_db/')
 
 def load_notes(folder="data"):
     file_list = []
@@ -46,11 +45,36 @@ def embed_chunks(chunk_list):
     print(embeddings.shape)
     
     return embeddings
+
+def ingest_notes():
+    collection = client.get_or_create_collection("notes")
+    notes = load_notes()
+    for doc in notes:
+        chunks = chunk_text(doc["text"])
+        chunk_embeddings = embed_chunks(chunks)
+        chunk_id_list = []
+        for i in range(0, len(chunks)):
+            chunk_id_list.append(f"{doc['filename']}_chunk_{i}")
+    
+        collection.add(ids = chunk_id_list, 
+                       documents = chunks, 
+                       embeddings = chunk_embeddings.tolist(),
+                       metadatas = [{"source": doc["filename"]} for _ in chunks])
+        print(f"Stored {len(chunks)} chunks from {doc['filename']}")
+            
+def test_retrieval():
+    collection = client.get_or_create_collection("notes")
+    query_emb = model.encode(["What is supervised learning?"]).tolist()
+    results = collection.query(
+        query_embeddings = query_emb,
+        n_results = 3
+    )
+    
+    for i, doc in enumerate(results["documents"][0]):
+        source = results["metadatas"][0][i]["source"]
+        print(f"\nResult {i+1} from {source}:\n{doc[:200]}")
             
 
 if __name__ == "__main__":
-    notes = load_notes()
-    chunks = chunk_text(notes[0]["text"])
-    print(f"Number of chunks: {len(chunks)}")
-    embeddings = embed_chunks(chunks)
-    
+    ingest_notes()
+    test_retrieval()
